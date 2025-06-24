e806f8381e7dceeac4e8c7e31cfda695d3b309aa
yukenz <yuyun.purniawan@gmail.com>
2025-06-24 20:50:25 +0700

    Add Eruda Provider

---
+++ app/provider/ErudaProvider.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React, {useEffect} from "react";
+
+type ErudaProviderProps = {
+    children: React.ReactNode;
+};
+
+export default function ErudaProvider({children}: ErudaProviderProps) {
+
+    useEffect(() => {
+        import("eruda").then(value => {
+            value.default.init();
+        });
+    }, []);
+
+    return children;
+}
